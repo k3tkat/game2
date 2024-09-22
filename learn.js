832feac4f8b482f9035f3db92a07e04333198e4f
Amandeep <amansiwach4444@gmail.com>
2024-09-22 16:02:26 +0530

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,698 @@
+/*name = "tony stark";
+console.log(fullName);
+
+
+const student = {
+    fullName : "aman",
+    age : 24,
+    subject : "computer science",
+    isPass : true,
+    percentage : 84.2,
+
+}
+
+student ["age"] = student ["age"] + 1;
+student ["fullName"] = "rahul lodu";
+console.log("fullName");
+
+
+const pen = {
+    fullName: "parker jotter",
+    Price: 270,
+    rating: 4,
+    priceOff: 5,
+    isDeal: true, 
+
+}
+
+pen ["Price"] = pen ["Price"] + 120;
+
+pen ["fullName"] = "aman";
+
+console.log(pen['fullName']);
+
+const insta = {
+    userName: "shardhaKhapra",
+    isFollow: true,
+    isMessage: true,
+    posts: 195,
+    followers: 569,
+    following: 4,
+
+}
+
+console.log(insta);*/
+
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a % b = ", a % b);
+
+// let a = 20;
+// let b = 45;
+
+// console.log("a ** b = ", a ** b);
+// let a = 5;
+// let b = 6;
+
+// console.log("a = " , 5 , " & b = " , 6 ,)
+// a++
+// console.log("a + b =", a + b );
+
+// let a = 5;
+// let b = 2;
+
+// a += 4;
+
+// console.log("a = " ,a,);
+
+// let a = 12;
+// let b = 14;
+
+// b -= 5;
+
+// console.log("b =" ,b,);
+
+// let a = 12;
+// let b = 14;
+
+// b **= 5;
+
+// console.log("b =" ,b,);
+
+// let a = 12;
+// let b = 12;
+
+// console.log("a == b" , a ==b);
+
+
+// let a = 12;
+// let b = "11";
+
+// b !== 5;
+
+// console.log("a !== b" , a !== b);
+
+// let a = 12;
+// let b = 11;
+
+// console.log(" a <= b" ,a <= b,);
+
+
+// let a = 11;
+// let b = 12;
+
+// console.log(" a >= b" ,a >= b);
+
+
+// let a = 6;
+// let b = 5;
+
+
+
+// console.log("cond1 && cond2 = ", a < b && a === 6); 
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// }
+
+// if (mode === "light"){
+//     color = "white";
+// }
+
+
+// console.log(color);
+
+
+// let mode = "light";
+// let color ;
+ 
+// if(mode === "dark"){
+//     color = "black"
+// }
+
+// if (mode === "light"){
+//     color = "white"
+// }
+
+// console.log(color);
+
+
+
+
+// let mode = "dark";
+
+// if(mode === "dark"){
+//     color = "black"
+// }
+
+// else{ color = "white"}
+
+// console.log(color)
+
+
+// let age = "17";
+// let vote ;
+
+// if(age >= "18"){
+//     console.log(" can vote")
+// }
+
+// else{
+//    console.log("can not vote")
+
+// }
+
+// let num = 255556778 ;
+
+// if(num%2 === 0){
+//     console.log("odd")
+// }
+
+// else{ 
+//     console.log("even")
+// }
+
+// let age = 81;
+
+// if (age < 20){
+//     console.log("younger");
+// }
+
+// else if ( age >= 80){
+//     console.log("older");
+// }
+
+// else {
+//     console.log("middle");
+// }
+
+
+// let mode = "dark";
+// color ;
+
+// if(mode === "dark"){
+//     console.log(color = "black ")
+// }
+
+// else if ( mode === "moody"){
+//     console.log(color = "pink")
+// }
+
+// else if ( mode === "hot" ){
+//     console.log(color = "red")
+// }
+
+// else {
+//     console.log(color = "white")
+
+// }
+
+
+// let mode = "hgh";
+
+// if(mode === "dark"){
+//     color = "black"
+// }
+
+// else if ( mode === "moody"){
+//     color = "pink"
+// }
+
+// else if ( mode === "hot" ){
+//     color = "red"
+// }
+
+// else {
+//     console.log(color = "white")
+
+// }
+
+// console.log(color);
+
+// let age = 25;
+
+// let result = age >= 18 ?  "adult" : "not adult";
+
+// console.log(result);
+
+// let age = 21;
+
+// let result = age >= 18 ? "adult" : "not adult";
+
+// console.log(result);
+
+
+// let grade = 90;
+
+// if ( grade >= 90 === 100 ){
+//     console.log("A Grade")
+// }
+
+// else if ( grade === 70 >= 89){
+//     console.log("B grade")
+// }
+
+// else if ( grade === 60 >= 69){
+//     console.log("Grade C")
+// }
+
+// else if ( grade === 50 >= 59){
+//     console.log(" Grade D")
+// }
+
+// else {
+//     console.log("fail")
+// }
+    
+
+// let student = 90;
+
+
+// if ( sudent = 90 >= 100 ){
+//      grade = "A"
+// }
+
+// else if ( sudent = 70 >= 89){
+//     grade = "B"
+// }
+
+// else if ( sudent = 60 >= 69){
+//     grade = "c"
+// }
+
+// else if ( sudent = 50 >= 59){
+//     grade = "D"
+// }
+
+// else {
+//     console.log("fail")
+// }
+
+// loops
+
+
+// for(let i = 3 ; i <= 50; i++){
+//     console.log("aman")
+// }
+// let sum = 0 ;
+// for (let i = 1 ; i <= 944 ; i++){
+//     sum = sum + i;
+// }
+// console.log("sum = ", sum);
+
+
+// for (let  i = 1; i <= 5; i++)
+
+
+// console.log("i=", ["aman"])
+
+// console.log("counting ends")
+
+// let sum = 0;
+
+// for(let i = 1; i <= 5; i++){
+//     sum = sum + i;
+// }
+// console.log("sum =", sum);
+
+
+// let i = 1;
+// do{
+//     console.log("i=", i);
+//     i++;
+// }while (i <= 5);
+
+// let str = "amansiwach";
+// let words = 0;
+
+// for (let i of str){
+//   console.log("i=", i);
+//     words++;  
+// }
+
+// console.log("string words =", words);
+
+
+
+// let me = "amansiwach"
+// let totalwords = 0;
+
+// for(let i of  me){
+//     console.log("i=", i);
+//     totalwords++
+// } console.log("i totalwords=",totalwords);
+
+// let student = {
+//     fullName: "amansiwach",
+//     age: 23,
+//     marks: 87,
+//     isPass: true,
+// }
+
+// for(let key in student){
+//     console.log("key=", key, "value=", student[key])
+// }
+
+// let num = 100;
+
+ 
+
+//  for(let i = 1 ; i <= 100; i++){
+//     console.log(i)
+//  }
+// console.log("thats it")
+
+// for(let i = 0; i <= 100; i++){
+//     if(i%2 !== 0){
+//         console.log(i)
+//     }
+// }
+
+// let gameNo = 25;
+
+// let userNo = (prompt("guss the gameNo: "));
+// console.log(userNo);
+
+// while(userNo !== gameNo){
+//    userNo = Number(prompt("you enter the wrong no. guess it again :")); 
+// }
+// console.log("you entered a right no")
+
+// let gameNo = 25;
+
+// let userNo = Number(prompt("Guess the gameNo: "));
+// console.log(userNo);
+
+// while(userNo !== gameNo){
+//    userNo = Number(prompt("You entered the wrong number. Guess again:"));
+// }
+// console.log("You entered the right number!");
+
+//   let student = ["tony", "mark", "hulk", "gojo", "aman"];
+
+//   let marks =[97, 80, 20, 70, 87];
+  
+//   for(let i = 0 ; i <= student.length; i++){
+//   console.log(student[i])
+//   }
+
+//   for(let i of marks ){
+//     console.log(i);
+//   }
+
+// let states =[ "mumbai","delhi","haryna","kolkata"];
+
+// for (let i of states){
+//     console.log(i)
+// }
+
+
+// let states = [ "mumbai","delhi","haryna","kolkata"];
+
+// for (let i = 0; i <= states.length; i++){
+//     console.log(states[i])
+// }
+
+// let lists = [12, 144, 44, 56, 49];
+
+// lists.pop(2,0);
+
+// for(let i of lists){
+//     console.log(i)
+// }
+
+// let marvelHeroes = ["spider","hulk","ironman","tony"];
+// let dcHeroes = ["superman", "batman", "supergirl", "flash" ];
+// let indHeroes = ["saktiman", "etc"]
+
+
+// let heroes = marvelHeroes.concat(dcHeroes , indHeroes);
+// console.log(heroes);
+
+// console.log(marvelHeroes.slice(0,3))
+
+// let arr = [ 1, 2, 3 , 4 , 5 , 6 ,7];
+
+// arr.splice(2,4,);
+// console.log(arr)
+
+// function aman(aman1){
+//     console.log("amna1")
+//     console.log("aman2")
+// }
+
+
+// aman("i am aman")
+
+// function sum (a , b){
+ //     console.log(a + b)
+
+// }
+// sum(4 , 6)
+
+// function sum (a , b){
+//     s = a + b;
+//      return s;
+
+// }
+// let val = sum(3, 4);
+// console.log(val);
+
+//   console.dir(document.body);
+//   console.log(document.body)
+
+// document.body.style.backgroundColor = "red";
+ 
+// document.body.childNodes[3].innerText="aman"
+
+// let some = document.querySelector("p");
+// console.log(some);
+// console.dir(some);
+
+// some.tagName
+
+// const div = document.querySelector("div");
+// console.dir(div);
+
+// div.innerText=("abcd");
+
+// let h3tag = document.querySelector("h3");
+// console.dir(h3tag);
+
+// h3tag.innerText=("aman");
+
+// h3tag.innerHTML="<i>jaat</i>"
+
+// let div = document.querySelector("div");
+// console.log(div);
+// let id = div.getAttribute("id");
+// console.log(id)
+
+// let div = document.querySelector("div");
+// console.log(div.setAttribute("id", "newbox"))
+
+// div.style.backgroundColor="green"
+// div.style.fontSize="20px"
+// div.style.border="50px solid black"3
+
+// let div = document.querySelector("div")
+//     console.log(div);
+//     div.style.height="40vw";
+//     div.style.width="20vw";
+//     div.style.border="2px solid black";
+//     div.style.backgroundColor="red";
+    
+
+
+//  let newBtn = document.createElement("button");
+//   newBtn.innerText=("click me");
+//   newBtn.style.width = ("80px")
+ 
+// console.log(newBtn);
+// div.before(newBtn);
+
+// let parent = document.getElementById("box");
+// let child = document.getElementById("child2");
+// let thro = parent.removeChild(child);
+  
+
+
+
+
+
+// let removed = div.removeChild(div.firstChild)
+
+
+// let div = document.querySelector("div");.
+
+// let btn = document.createElement("button")
+// console.log(btn)
+
+// let paragraph = document.body.appendChild(document.createElement("p"));
+
+
+
+
+// This doesn't append three paragraphs:
+// the three elements will be nested instead of siblings
+
+// let div = document.getElementById("box");
+// let btn = document.createElement("button");
+// btn.innerText="button";
+// btn.style.color="red";
+// btn.style.backgroundColor="pink";
+// btn.style.width="50px";
+// btn.style.height="100px"
+// document.body.appendChild(btn);
+
+// btn.addEventListener("click", function(){
+//     alert("button clicked")
+
+//     btn.style.color = "pink;"
+//     btn.style.backgroundColor = "blue";
+    
+
+
+// });
+
+// let para = document.getElementById("content");
+
+//     para.setAttribute("class","newclass");
+//     para.classList.add("para1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let btn = document.getElementById("mode")
+// let thisMode = "light";
+
+// btn.addEventListener("click", function(){
+//     if  (thisMode === "light"){
+//         thisMode = "dark";
+//         document.querySelector("body").style.backgroundColor = "pink";
+//     }
+
+//     else {
+//         thisMode = "light";
+//         document.querySelector("body").style.backgroundColor = "white";
+//     }    
+
+//     console.log(thisMode);
+// })
+
+
+// game******************************
+
+
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.getElementById("msg");
+const msg2 = document.getElementById("winLose");
+const userscore = document.getElementById("userscore0");
+const computerscore = document.getElementById("computerscore0");
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissor"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+    
+};const drawGame = () => {
+    console.log("game was draw.");
+
+};
+
+const showWinner = (userWin) => {
+    if(userWin){
+        userScore++;
+        userscore0.innerText = userScore;
+        console.log("you win!");
+        msg.innerText = "you Win!";
+        winLose.style.backgroundColor = "yellow";
+    }
+    else{
+        compScore++;
+        computerscore0.innerText = compScore;
+        console.log("you lose!");
+        msg.innerText = "you Lose!"
+        winLose.style.backgroundColor = "red";
+        
+    }
+}
+const playGame = (userChoice) => {
+        console.log("user choice =", userChoice);
+        const compChoice = genCompChoice();
+        console.log("comp choice =", compChoice);
+
+        if(userChoice === compChoice){
+            drawGame();
+            msg.innerText = "draw!"
+            winLose.style.backgroundColor = "brown";
+        }
+        else{
+            let userWin = true;
+            if(userChoice === "rock"){
+                userWIN = compChoice === "paper" ? false : true;
+            }
+            else if(userChoice === "paper") {
+                userWin = compChoice === "scissor" ? false : true;
+                 
+            }
+            else{
+            
+                    userWin = compChoice === "rock" ? false : true;
+                }
+
+                showWinner(userWin);
+        }
+    };
+
+
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
